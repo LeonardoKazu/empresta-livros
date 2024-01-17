1210b9e18f2e083e5eb7e216f0d856cdddf44fbe
Leonardo Kazu <kazuyoshikad@gmail.com>
2024-01-17 16:03:52 -0300

    Criado Livro e LivroDTO + config DB

---
+++ src/main/java/com/leonardokazu/livraria/entities/DTOS/LivroDTO.java
@@ -0,0 +1,4 @@
+package com.leonardokazu.livraria.entities.DTOS;
+
+public record LivroDTO(String nome, String autor, Integer totalPaginas) {
+}
---
+++ src/main/java/com/leonardokazu/livraria/entities/Livro.java
@@ -0,0 +1,22 @@
+package com.leonardokazu.livraria.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String autor;
+    private Integer totalPaginas;
+    private boolean disponivel = true;
+}
