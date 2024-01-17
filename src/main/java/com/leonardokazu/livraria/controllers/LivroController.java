1a063c96e70a2685d68929db16dfc5a207e37761
Leonardo Kazu <kazuyoshikad@gmail.com>
2024-01-17 20:09:03 -0300

    Implementado para LivroController e LivroService: Get, Put, Post e Delete

--- src/main/java/com/leonardokazu/livraria/controllers/LivroController.java
+++ src/main/java/com/leonardokazu/livraria/controllers/LivroController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/livro")
 public class LivroController {
@@ -19,5 +21,25 @@ public class LivroController {
     public ResponseEntity<Livro> salvar(@RequestBody LivroDTO livroDTO){
 
         return ResponseEntity.status(HttpStatus.CREATED).body(livroService.salvar(livroDTO));
+    }
+    @GetMapping
+    public ResponseEntity<List<Livro>> lerTodos(){
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.lerTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> lerPorId(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.lerPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Livro> atualizarPorId(@RequestBody LivroDTO livroDTO, @PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.atualizar(livroDTO, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPorId(@PathVariable Long id){
+        livroService.deletarPorId(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/leonardokazu/livraria/services/LivroService.java
+++ src/main/java/com/leonardokazu/livraria/services/LivroService.java
@@ -6,6 +6,8 @@ import com.leonardokazu.livraria.repositories.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LivroService {
 
@@ -20,5 +22,26 @@ public class LivroService {
         livro.setTotalPaginas(livroDTO.totalPaginas());
 
         return livroRepository.save(livro);
+    }
+
+    public List<Livro> lerTodos(){
+        return livroRepository.findAll();
+    }
+
+    public Livro lerPorId(Long id){
+        return livroRepository.findById(id).get();
+    }
+
+    public Livro atualizar(LivroDTO livroDTO, Long id){
+        Livro livro = livroRepository.findById(id).get();
+
+        livro.setNome(livroDTO.nome());
+        livro.setAutor(livroDTO.autor());
+        livro.setTotalPaginas(livroDTO.totalPaginas());
+
+        return livroRepository.save(livro);
+    }
+    public void deletarPorId(Long id){
+        livroRepository.deleteById(id);
     }
 }
